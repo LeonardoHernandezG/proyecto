82148763aa8d0e2383e5b616db052cb32f43237c
LeonardoHernandezG <111701901+LeonardoHernandezG@users.noreply.github.com>
2023-04-29 01:47:12 -0600

    si funciona

--- assembler.js
+++ assembler.js
@@ -57,25 +57,3 @@ function analizarCodigo() {
   areaErrores.value = analisis.errores;
 }
 
-
-function insertarEjemplo() {
-    var ejemplo = "section .data\n" +
-                  "    mensaje db 'Hola, mundo!', 0\n" +
-                  "\n" +
-                  "section .text\n" +
-                  "    global _start\n" +
-                  "\n" +
-                  "_start:\n" +
-                  "    ; Escribe el mensaje en la consola\n" +
-                  "    mov eax, 4\n" +
-                  "    mov ebx, 1\n" +
-                  "    mov ecx, mensaje\n" +
-                  "    mov edx, 13\n" +
-                  "    int 0x80\n" +
-                  "\n" +
-                  "    ; Salida del programa\n" +
-                  "    mov eax, 1\n" +
-                  "    xor ebx, ebx\n" +
-                  "    int 0x80";
-    document.getElementById("codigo").value = ejemplo;
-  }
