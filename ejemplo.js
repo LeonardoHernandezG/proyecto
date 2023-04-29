e5927e78d69c0bf9bd667a3f997535dff97638af
LeonardoHernandezG <111701901+LeonardoHernandezG@users.noreply.github.com>
2023-04-29 02:06:42 -0600

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -7,17 +7,17 @@ function insertarEjemplo() {
         global _start
     
     _start:
-        ; Escribir mensaje en pantalla
-        mov eax, 4 ; Número de la llamada del sistema para escribir
-        mov ebx, 1 ; Descriptor de archivo para stdout
-        mov ecx, msg ; Dirección del mensaje
-        mov edx, 13 ; Longitud del mensaje
-        int 0x80 ; Llamada al sistema para escribir en la pantalla
+        
+        mov eax, 4 ;
+        mov ebx, 1 ;
+        mov ecx, msg ; 
+        mov edx, 13 ;
+        int 0x80 ;
     
-        ; Salida del programa
-        mov eax, 1 ; Número de la llamada del sistema para salir
-        xor ebx, ebx ; Código de retorno (0)
-        int 0x80 ; Llamada al sistema para salir
+       
+        mov eax, 1 
+        xor ebx, ebx 
+        int 0x80 
     `;
   
     document.getElementById('codigo').value = ejemplo;
